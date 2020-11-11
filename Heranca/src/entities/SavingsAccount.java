2fdaebbf3a8c59747dd90dbee92f02625eb1f1b3
Herick Exterkoetter <herickherick47@gmail.com>
2020-11-11 04:41:05 -0300

    :fire: feat: add the class SavingsAccount, and concepts on upcasting and downcasting

---
+++ Heranca/src/entities/SavingsAccount.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class SavingsAccount extends Account{
+	
+	private Double interestRate;
+	
+	public SavingsAccount() { //construtor padrão da classe account
+		super();
+	}
+
+	public SavingsAccount(Integer number, String holder, double balance, Double interestRate) {
+		super(number, holder, balance);
+		this.interestRate = interestRate; // complementando atribuindo interestRaTE
+	}
+
+	public Double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(Double interestRate) {
+		this.interestRate = interestRate;
+	}
+	
+	public void updateBalance() {
+		balance += balance * interestRate;
+	}
+	
+}
